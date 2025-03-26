0b5c2e981cf3803d676b9e357b3a55af75e36b32
Silas Souza <93294965+silas-souza@users.noreply.github.com>
2025-03-26 15:22:26 -0300

    Add files via upload

---
+++ ex.ts
@@ -0,0 +1,16 @@
+    
+    function multiplicar(num1: number, num2: number): number {
+        return num1 * num2;
+    }
+    
+
+    function saudar(nome: string): string {
+        return "Olá " + nome;
+    }
+    
+    
+    const resultadoMultiplicacao = multiplicar(5, 10);
+    console.log(resultadoMultiplicacao); // Saída: 50
+    
+    const saudacaoPessoa = saudar("João");
+    console.log(saudacaoPessoa); // Saída: Olá João
